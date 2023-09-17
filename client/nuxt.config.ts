d399edb2830348f5676a2b2dee907e001a5cfac8
Joompon Sajjaphoch <joom.sajja@gmail.com>
2023-09-18 05:22:13 +0700

    Add TailwindCSS and daisyUI

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/devtools'],
+  modules: ['@nuxt/devtools', '@nuxtjs/tailwindcss'],
   devtools: { enabled: true },
 })
